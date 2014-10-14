81425be68b7445fb54940598f4ae03f4efebf0e3
shigemk2 <i.am.shige@gmail.com>
2014-10-14 22:32:31 +0900

    オブジェクトを生成

---
+++ 1-1.js
@@ -0,0 +1,8 @@
+// オブジェクトとは、名前と値を持つプロパティを格納するコンテナにすぎない
+var cody = new Object();
+
+cody.living = true;
+cody.age = 33;
+cody.gender = 'male';
+
+console.log(cody); // { living: true, age: 33, gender: 'male' }
