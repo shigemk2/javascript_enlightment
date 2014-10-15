e46dc97e0e86b255a2bd61d6f7e06a7c73f8c879
shigemk2 <i.am.shige@gmail.com>
2014-10-16 00:10:02 +0900

    メソッドとプロパティ

---
+++ 1-2.js
@@ -0,0 +1,9 @@
+// コンストラクタ関数=テンプレート、クッキーの抜き型
+var cody = new Object();
+cody.living = true;
+cody.age = 33;
+cody.gender = 'male';
+cody.getGender = function() {
+  return cody.gender;
+};
+console.log(cody.getGender()); // male
