dcde3633759383ef79368a5843349c2262f1ef3a
shigemk2 <i.am.shige@gmail.com>
2014-10-16 22:53:51 +0900

    2つのコンストラクタを使ってインスタンスを生成する

---
+++ 1-5.js
@@ -0,0 +1,33 @@
+// Object()コンストラクタを使ってcodyAを生成
+var codyA = new Object();
+codyA.living = true;
+codyA.age = 33;
+codyA.gender = 'male';
+codyA.getGender = function() {
+  return codyA.gender;
+};
+
+console.log(codyA); // { living: true, age: 33, gender: 'male', getGender: [Function] }
+
+/* 次の例では、上の例と同じオブジェクトを生成しますが、次はObject()コンストラクタで一度きりのcodyを生成するのではなく、最初にcodyオブジェクト（もしくは他のPersonオブジェクト）を生成するPerson()コンストラクタを定義しnewを使ってインスタンス化します。 */
+
+var Person = function(living, age, gender) {
+  this.living = living;
+  this.age = age;
+  this.gender = gender;
+  this.getGender = function() { return this.gender; };
+};
+
+var codyB = new Person(true, 33, 'male');
+
+console.log(codyB); // { living: true, age: 33, gender: 'male', getGender: [Function] }
+
+
+
+
+
+
+
+
+
+
