19e7312b812b449d4446c1738a60fde7d4e6acaa
shigemk2 <i.am.shige@gmail.com>
2014-10-20 22:05:38 +0900

    コンストラクタ関数によるオブジェクトの生成、もしくはインスタンス化

---
+++ 1-7.js
@@ -0,0 +1,19 @@
+// http://jsfiddle.net/jseja/DZ6Pv
+// myArrayという配列オブジェクトをインスタンス化する。
+var myArray = new Array(); // myArrayは配列のインスタンス
+
+// myArrayはオブジェクトであり、Array()コンストラクタのインスタンス
+console.log(typeof myArray);
+
+console.log(myArray);
+console.log(myArray.constructor);
+
+
+
+
+
+
+
+
+
+
