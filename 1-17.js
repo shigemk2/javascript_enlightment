2783b3b33cf05920107208ef5630eee32345214a
shigemk2 <i.am.shige@gmail.com>
2014-10-22 00:31:15 +0900

    ネイティブコンストラクタによって生成されるオブジェクトは、ひとつ以上のプリミティブ値もしくはオブジェクトを格納することができる

---
+++ 1-17.js
@@ -0,0 +1,23 @@
+// http://jsfiddle.net/jseja/KAERr
+var object = {
+    myString: 'string',
+    myNumber: 10,
+    myBoolean: false,
+    myNull: null,
+    myUndefined: undefined
+};
+
+console.log(object);
+
+var array = ['string', 10, false, null, undefined];
+
+console.log(array);
+
+/* このオブジェクトと配列を、プリミティブ値のシンプルさとくらべてみてください。プリミティブ値は下記よりも複雑にすることはできませんが、オブジェクトや配列といった複雑な値はどのようなJavaScriptの値をいくつでも格納することができます。 */
+
+var myString    = 'string';
+var myNumber    = 10;
+var myBoolean   = false;
+var myNull      = null;
+var myUndefined = undefined;
+
