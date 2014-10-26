5fd55337e1afc8fc97473d9193627cafea8e083e
shigemk2 <i.am.shige@gmail.com>
2014-10-26 22:47:41 +0900

    何だこの改行

--- 1-25.js
+++ 1-25.js
@@ -9,21 +9,3 @@ console.log(instanceofCustomObject.constructor === CustomConstructor);
 
 // CustomConstructor()関数へのリファレンスを返す
 console.log(instanceofCustomObject.constructor);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
