a10cdc0dfaa2d0c23f608ed3f259c8a3a9ab48ef
shigemk2 <i.am.shige@gmail.com>
2014-10-31 00:18:12 +0900

    Array()オブジェクトでも他のオブジェクトを格納できる オブジェクトは、名前のついた値（プロパティ）の集合体を格納するコンテナ

---
+++ 2-3.js
@@ -0,0 +1,27 @@
+// 配列を使って擬似的な多次元配列を生成
+
+var myArray = [[[]]]; // 空の配列に空の配列を持った空の配列
+
+// 関数を使ったカプセル化の例。空の関数の中に空の関数を持った空の関数
+var myFunction = function() {
+  // 空
+  var myFunction = function() {
+    // 空
+    var myFunction = function() {
+      // 空
+    };
+  };
+};
+
+// オブジェクトと配列をごちゃごちゃに混ぜてから取り出すことも可能
+var foo = [{
+  foo : [{
+    bar: {
+      say :function() {
+        return 'hi';
+      }
+    }
+  }]
+}];
+
+console.log(foo[0].foo[0].bar.say()); // hi
