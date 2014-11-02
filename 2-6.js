ae3d7164f03e1a3cf80787428eb41b5d64cde427
shigemk2 <i.am.shige@gmail.com>
2014-11-02 15:18:53 +0900

    ドット記法では実現不可能な操作

---
+++ 2-6.js
@@ -0,0 +1,16 @@
+// http://jsfiddle.net/jseja/BzTNU
+var foobarObject = {foobar: 'hogehoge'};
+var string1 = 'foo';
+var string2 = 'bar';
+
+console.log(foobarObject[string1 + string2]); // ドット記法では実現不可能な操作
+
+
+
+
+
+
+
+
+
+
