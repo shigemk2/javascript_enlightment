a388dae204f0a2ee734a01c926e778344283d20e
shigemk2 <i.am.shige@gmail.com>
2014-11-02 15:34:21 +0900

    コメント追加

--- 2-7.js
+++ 2-7.js
@@ -8,3 +8,13 @@ console.log(myObject['123'], myObject['class']); // 'zero foo'
 // ドット表記法ではおなじことはできない
 // 次の行のコメントを外して実行するとエラーを出力する
 // console.log(myObject.0, myObject.class);
+
+// オブジェクトは他のオブジェクトを格納することができるので、たとえば
+// cody.object.object.object.objectや、
+// cody['object']['object']['object']['object']
+// といった表記に遭遇することもあります。
+
+// オブジェクトは無制限に階層化することができます。
+
+// JavaScriptのオブジェクトはミュータブル（可変）です。つまり、ほとんどのオブ
+// ジェクトに対して、いつでも取得・設定・更新を行うことができるということです
