d3c694e60885452580f8d5bf636abe2824d0ba19
shigemk2 <i.am.shige@gmail.com>
2014-11-02 15:52:32 +0900

    JavaScriptはmyArray.fooが存在しないことを確認後、次にArray.prototype.fooを探す

--- 2-9.js
+++ 2-9.js
@@ -1,5 +1,13 @@
 // http://jsfiddle.net/jseja/MTApU
-var myArray = [];
-console.log(myArray.foo); // undefined
-
 /* JavaScriptはmyArray.fooが存在しないことを確認後、次にArray.prototype.fooを探す。しかしこのプロパティも存在しないため、次にObject.prototype.fooを探す。これも存在しないため、undefinedを返す。 */
+
+var myArray = [];
+Object.prototype.foo=function(val){
+    return 'fuga';
+};
+Array.prototype.foo=function(val){
+    return 'hoge';
+};
+console.log(myArray.foo()); // この場合hogeを返す
+console.log(myArray.foo); // この場合hogeを返す
+
