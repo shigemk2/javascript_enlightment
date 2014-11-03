c6183941c81452a2a0e62a2bc675d097ca5b23ba
shigemk2 <i.am.shige@gmail.com>
2014-11-04 00:21:19 +0900

    コメント追加

--- 2-16.js
+++ 2-16.js
@@ -5,7 +5,8 @@ var cody = {
 };
 
 for (var key in cody) {
-  if (cody.hasOwnProperty(key)) {
+  if (cody.hasOwnProperty(key)) { // // keyはループ内でそれぞれのプロパティ名を格納する変数
+    // プロトタイプチェーンから継承されたプロパティを除く
     console.log(key);
   }
 }
