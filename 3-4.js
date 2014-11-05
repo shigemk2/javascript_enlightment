60f61a9ea7b543a542290c8c4b9ed6332910ccba
shigemk2 <i.am.shige@gmail.com>
2014-11-06 00:22:59 +0900

    Object()コンストラクタ関数でcodyオブジェクトを生成

---
+++ 3-4.js
@@ -0,0 +1,7 @@
+// http://jsfiddle.net/jseja/mcAGj/
+var cody = new Object();
+cody.living = true;
+cody.age = 33;
+cody.gender = 'male';
+cody.getGender = function() {return cody.gender; };
+console.log(cody);
