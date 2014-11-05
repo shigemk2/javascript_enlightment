349eb4e34e93685dde0199e3e67abc0e9986ddff
shigemk2 <i.am.shige@gmail.com>
2014-11-06 00:51:03 +0900

    オブジェクトリテラル

---
+++ 3-5.js
@@ -0,0 +1,8 @@
+// http://jsfiddle.net/jseja/sCPHA/
+var cody = {
+  living: true,
+  age: 33,
+  gender: 'male',
+  getGender: function() { return cody.gender; }
+}; // 最後のプロパティの後ろにはカンマをつけない
+console.log(cody);
