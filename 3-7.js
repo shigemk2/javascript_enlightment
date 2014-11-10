d45210d3059eb8ab3d75d57f25607462bce4cb87
shigemk2 <i.am.shige@gmail.com>
2014-11-11 00:43:54 +0900

    コメント追加

--- 3-7.js
+++ 3-7.js
@@ -5,3 +5,6 @@ Object.prototype.foo = 'foo';
 var myString = "empty string";
 
 console.log(myString.foo); // foo
+
+
+// Object.prototypeに追加したものは、すべてのオブジェクトのプロトタイプチェーンに影響を与え、for-inループでも列挙対象となります。そのため、一般にObject.prototypeを編集することは禁止すべきだとも言われています。
