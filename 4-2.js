9b77f27c9b3bca1c3feded6a2c00d8ed90d4fba7
shigemk2 <i.am.shige@gmail.com>
2014-11-14 01:06:28 +0900

    コメント追加

--- 4-2.js
+++ 4-2.js
@@ -7,12 +7,4 @@ var timesFunction = new Function('num1, num2', 'return num1 * num2');
 
 console.log(addFunction(2,2), timesFunction(2,2)); // 4 4
 
-
-
-
-
-
-
-
-
-
+// 一般的に、Function()コンストラクタを直接使用することは推奨されておらず、通常この方法での関数生成は行われません。その理由は、JavaScriptはeval()を使って関数本体を解析するからです。eval()の実行は多くの場合に無駄なオーバーヘッドとして認識されています。もしコード内でFunction()コンストラクタが使われている場合は、設計を見なおした方が良いでしょう
