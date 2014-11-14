2286ebe8366bfa54aa4335229386717685a7e96c
shigemk2 <i.am.shige@gmail.com>
2014-11-15 03:00:02 +0900

    関数は指定されたステートメントをシンプルに実行するためだけに生成することもあるが、一般的には計算結果の値を返す

---
+++ 4-3.js
@@ -0,0 +1,16 @@
+// http://jsfiddle.net/jseja/VWz78/
+// 関数は指定されたステートメントをシンプルに実行するためだけに生成することもあるが、一般的には計算結果の値を返す。
+// 戻り値を指定していない場合はundefined
+var sayHi = function() {
+  return 'Hi';
+};
+
+console.log(sayHi()); // Hi
+
+
+
+
+
+
+
+
