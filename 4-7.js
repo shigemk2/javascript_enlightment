a2a0bb4516811ba643299bce2ad2dcf99cd9e923
shigemk2 <i.am.shige@gmail.com>
2014-11-17 22:57:58 +0900

    arguments

--- 4-7.js
+++ 4-7.js
@@ -9,3 +9,6 @@ myObject1.myMethod();
 var myObject2 = function() { console.log(this); };
 // thisはグローバルオブジェクト
 myObject2();
+
+var myObject3 = function() { console.log(arguments); };
+myObject3('hoge', 'fuga', 'bar'); // { '0': 'hoge', '1': 'fuga', '2': 'bar' }
