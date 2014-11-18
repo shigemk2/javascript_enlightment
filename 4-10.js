fd4191a6f40f4328d40d7346cc3286f2fffdd306
shigemk2 <i.am.shige@gmail.com>
2014-11-18 23:52:04 +0900

    引数が渡されていないため0が出力される

---
+++ 4-10.js
@@ -0,0 +1,16 @@
+// http://jsfiddle.net/jseja/BCc3p
+var myFunction = function(z, s, d) {
+  return arguments.length;
+};
+
+console.log(myFunction()); // 出力は0
+// 引数が渡されていないため0が出力される
+
+
+
+
+
+
+
+
+
