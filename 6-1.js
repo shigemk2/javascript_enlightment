49f192812ad253b18a8ce5f3407701140f45a4f0
shigemk2 <i.am.shige@gmail.com>
2014-11-24 23:35:35 +0900

    例のアレ

---
+++ 6-1.js
@@ -0,0 +1,9 @@
+// http://jsfiddle.net/jseja/KbVvW
+var cody = {
+	living : true,
+	age : 33,
+	gender : 'male',
+	getGender : function() { return cody.gender; }
+};
+
+console.log(cody.getGender()); // 出力：'male'
