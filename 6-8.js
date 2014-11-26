228d5419de1d2aef7675239cedb457f705dc5ac9
shigemk2 <i.am.shige@gmail.com>
2014-11-27 00:22:16 +0900

    関数内のthisの値がmyObjectとなるようcall()で関数を呼び出します

---
+++ 6-8.js
@@ -0,0 +1,12 @@
+// http://jsfiddle.net/jseja/G3kJp
+var myObject = {};
+
+var myFunction = function(param1, param2) {
+  // 後でcall()で呼ばれるときにthisはmyObjectを参照
+  this.foo = param1;
+  this.bar = param2;
+  console.log(this); // 出力: Object { foo = 'foo', bar = 'bar' }
+};
+
+myFunction.call(myObject, 'foo', 'bar'); // myFunction.call(myObject, 'foo', 'bar'); // myObjectをthisとしてmyFunctionを呼ぶ
+console.log(myObject); // 出力: Object { foo = 'foo', bar = 'bar' }
