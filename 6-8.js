a5a95e8555a6f071fa8b41902ff8efbb493021eb
shigemk2 <i.am.shige@gmail.com>
2014-11-28 21:11:31 +0900

    call

--- 6-8.js
+++ 6-8.js
@@ -10,3 +10,6 @@ var myFunction = function(param1, param2) {
 
 myFunction.call(myObject, 'foo', 'bar'); // myFunction.call(myObject, 'foo', 'bar'); // myObjectをthisとしてmyFunctionを呼ぶ
 console.log(myObject); // 出力: Object { foo = 'foo', bar = 'bar' }
+myFunction.call(myObject, 'hoge', 'fuga');
+console.log(myObject);
+myFunction.call(myObject, ['hoge', 'fuga']);
