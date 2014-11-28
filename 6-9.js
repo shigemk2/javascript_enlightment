2efe9793166233c1506f323e7789a1611d60865f
shigemk2 <i.am.shige@gmail.com>
2014-11-28 21:15:25 +0900

    コメント追加

--- 6-9.js
+++ 6-9.js
@@ -12,3 +12,4 @@ myFunction.apply(myObject, ['foo', 'bar'] ); // myObjectをthisとしてmyFuncti
 console.log(myObject);
 myFunction.apply(myObject, ['hoge', 'fuga'] );
 console.log(myObject);
+// call()を使う場合は引数はカンマ区切りの値ですが、apply()を使う場合は引数は配列に格納されて渡されます
